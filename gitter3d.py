2a3b4924d92e8e0ec0e85dd2a780977a7e7643a9
nomskybeat <naomi.glur@gmail.com>
2024-08-29 20:56:18 +0200

    gitter

--- gitter3d.py
+++ gitter3d.py
@@ -44,16 +44,28 @@ for s in range(anzahl_simulationen):
 def visualisiere_gitter(gitterliste):
     fig = plt.figure(figsize=(15, 5))
     
-    for idx, gitter in gitterliste:
+    for idx, gitter in enumerate(gitterliste):
         ax = fig.add_subplot(1, 3, idx + 1, projection='3d')
         for i in range(y):
             for j in range(x):
                 for k in range(z):
-                    ax.scatter(j, i, k, c='b', s=gitter[i, j, k] * 10)
+                    #ax.scatter(j, i, k, c='b', s=gitter[i, j, k] * 10)
+                    marker_size = gitter[i, j, k] * 10 if gitter[i, j, k] != 0 else 1
+                    # wenn der Punkt 0 ist, dann grün, sonst blau
+                    if gitter[i, j, k] == 0:
+                        ax.scatter(j, i, k, c='g', s=marker_size)
+                    else:
+                        ax.scatter(j, i, k, c='b', s=marker_size)
+        
         ax.set_xlabel('X Position')
         ax.set_ylabel('Y Position')
         ax.set_zlabel('Z Position')
         ax.set_title(f'Entropie Visualisierung Körper {idx + 1}')
+
+        ax.set_xticks(np.arange(0, x, 1))
+        ax.set_yticks(np.arange(0, y, 1))
+        ax.set_zticks(np.arange(0, z, 1))
+        ax.grid(True)
     
     plt.tight_layout()
     plt.show()
@@ -64,8 +76,8 @@ def visualisiere_gitter(gitterliste):
 # Körper 1 - anzahl_koerper als matrix definieren
 gitterliste = []
 for i in range(anzahl_koerper):
-    gitter = gitter[:, i:(i+x), :]
-    gitterliste.append(gitter)
+    gitter2 = gitter[:, i*(x):(i+1)*(x), :]
+    gitterliste.append(gitter2)
 visualisiere_gitter(gitterliste)
 
 # durchschnitt berechnen
